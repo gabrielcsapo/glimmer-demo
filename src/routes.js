a3a65964aaab1c6cb4b4959298dd2ee674847b7a
Lewis Miller <lemiller@linkedin.com>
2023-02-21 16:13:15 -0800

    feat: lazy load about route

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,42 @@
 import { renderComponent } from "@glimmerx/core";
 
 import Home from "./pages/Home.gjs";
-import About from "./pages/About.gjs";
 import Contact from "./pages/Contact.gjs";
 
+let AboutRoute = null;
+
 export default function (element) {
-  return {
-    about: () => {
-      element.innerHTML = "";
-      renderComponent(About, element);
+  return [
+    {
+      path: "about",
+      handler: () => {
+        element.innerHTML = "";
+        renderComponent(AboutRoute, element);
+      },
+      hooks: {
+        before: (done) => {
+          console.log(`before about`);
+          return import("./pages/About.gjs").then((module) => {
+            AboutRoute = module.default;
+            done();
+          });
+        },
+      },
     },
-    contact: () => {
-      element.innerHTML = "";
-      renderComponent(Contact, element);
+    {
+      path: "contact",
+      handler: () => {
+        element.innerHTML = "";
+        renderComponent(Contact, element);
+      },
     },
-    "*": () => {
-      console.log("hi");
-      element.innerHTML = "";
-      renderComponent(Home, element);
+    {
+      path: "*",
+      handler: () => {
+        console.log("hi");
+        element.innerHTML = "";
+        renderComponent(Home, element);
+      },
     },
-  };
+  ];
 }
